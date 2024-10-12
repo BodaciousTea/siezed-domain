106e9227bccc46028cd2ef2a178094581f6551fc
bodacious <84815738+BodaciousTea@users.noreply.github.com>
2024-10-12 17:06:39 -0500

    Delete freebobyuxr directory

--- freebobyuxr/assets.trendndaily.com/js/dynamic-disclaimer.js
+++
@@ -1,3 +0,0 @@
-"use strict";class DynamicDisclaimer{constructor(ctaText,app){this.app=app||window.appInstance||new App();this.ctaText=ctaText;}
-onInit(){if(window.isDynamicDisclaimerRendered)return;this.injectDisclaimer();}
-injectDisclaimer(){if(document.querySelector('#cta-disclaimer')){var _window$customData;this.ctaText=this.ctaText.replaceAll('&nbsp;','').trim();var disclaimer=((_window$customData=window.customData)===null||_window$customData===void 0?void 0:_window$customData.lander_disclaimer)||this.app.getFallbackDisclaimer();document.querySelector('#cta-disclaimer').innerHTML=decodeURIComponent(disclaimer.replace('{{ctaText}}',encodeURIComponent(ctaText)));}}}
